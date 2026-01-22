4060151db2f0e4dc893ae9334132091872514020
Ryan Wade <ryanwade628@gmail.com>
2026-01-22 10:55:43 -0500

    Update networking, enable WoL and Sunshine

--- hosts/global/common/gaming/default.nix
+++ hosts/global/common/gaming/default.nix
@@ -9,6 +9,7 @@
     ./gamemode.nix
     ./lutris.nix
     ./steam.nix
+    ./sunshine.nix
   ];
 
   environment.systemPackages = with pkgs; [
--- hosts/x86/nixos/config/networking.nix
+++ hosts/x86/nixos/config/networking.nix
@@ -1,11 +1,30 @@
 {
+  networking = {
+    interfaces = {
+      enp42s0 = {
+        wakeOnLan.enable = true;
+      };
+    };
+    firewall = {
+      allowedUDPPorts = [ 9 ];
+    };
+  };
   systemd.network.networks."10-enp42s0" = {
     matchConfig.Name = "enp42s0";
     networkConfig = {
-      DHCP = "ipv4";
+      # DHCP = "ipv4";
       VLAN = "enp42s0.50";
     };
-    dhcpV4Config.RouteMetric = 100;
+    # dhcpV4Config.RouteMetric = 100;
+    address = [
+      "10.0.10.4/24"
+    ];
+    routes = [
+      { Gateway = "10.0.10.1"; }
+    ];
+    dns = [ "10.0.10.1" ];
+    # make the routes on this interface a dependency for network-online.target
+    linkConfig.RequiredForOnline = "routable";
   };
 
   systemd.network.netdevs."enp42s0.50" = {
