799d42e5b25ecc08b972529a5ed27ca3c5ccc61c
Ryan Wade <ryanwade628@gmail.com>
2026-04-21 08:49:25 -0400

    Update lazyvim

--- modules/home/core/neovim/lazyvim/plugins/utils.lua
+++ modules/home/core/neovim/lazyvim/plugins/utils.lua
@@ -1,6 +1,19 @@
 return {
-  -- add tmux navigation
-  { "alexghergh/nvim-tmux-navigation", opts = {
-    disable_when_zoomed = true,
-  } },
+	-- add tmux navigation
+	{
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			require("nvim-tmux-navigation").setup({
+				disable_when_zoomed = true, -- defaults to false
+				keybindings = {
+					left = "<C-h>",
+					down = "<C-j>",
+					up = "<C-k>",
+					right = "<C-l>",
+					last_active = "<C-\\>",
+					next = "<C-Space>",
+				},
+			})
+		end,
+	},
 }
