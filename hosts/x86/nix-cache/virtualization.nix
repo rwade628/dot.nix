aa50440f5ae0a15915d0b2913c16d8d2b2131dc0
Ryan Wade <ryanwade628@gmail.com>
2026-04-28 13:58:08 -0400

    Update config

--- hosts/x86/nix-cache/virtualization.nix
+++ hosts/x86/nix-cache/virtualization.nix
@@ -7,4 +7,9 @@
   virtualisation.libvirtd.enable = true;
   virtualisation.incus.enable = true;
   programs.virt-manager.enable = true;
+
+  # TODO move off incus/truenas
+  nixpkgs.config.permittedInsecurePackages = [
+    "minio-2025-10-15T17-29-55Z"
+  ];
 }
