43a98a4a4b4f24ad688891fe0bfd5dd59d1edd44
Ryan Wade <ryanwade628@gmail.com>
2026-04-29 16:01:00 -0400

    Add DNS config

--- hosts/x86/nix-cache/networking.nix
+++ hosts/x86/nix-cache/networking.nix
@@ -10,4 +10,17 @@
   networking.firewall.allowedTCPPorts = [
     5000 # Harmonia binary cache
   ];
+
+  services.resolved = {
+    enable = true;
+    settings.Resolve = {
+      Domains = [
+        "~local"
+        "~casadewade.com"
+      ]; # Route local zones to our DNS
+      DNS = [
+        "10.0.10.1"
+      ];
+    };
+  };
 }
